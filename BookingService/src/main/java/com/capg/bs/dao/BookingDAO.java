0b244d9d80ed122ff90b84df95b03c56cc69aab5
PallaviReddy27 <62416664+PallaviReddy27@users.noreply.github.com>
2021-01-20 14:48:14 +0530

    Delete BookingService directory

--- BookingService/src/main/java/com/capg/bs/dao/BookingDAO.java
+++
@@ -1,9 +0,0 @@
-package com.capg.bs.dao;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import com.capg.bs.dto.Booking;
-@Repository
-public interface BookingDAO extends JpaRepository<Booking,Long>
-{
-
-}
