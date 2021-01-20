0cfadc7cb926c9ee06b0789c629faa8572b0a81f
PallaviReddy27 <62416664+PallaviReddy27@users.noreply.github.com>
2021-01-20 14:49:02 +0530

    Delete FlightService directory

--- FlightService/src/main/java/com/capg/fs/dao/FlightDAO.java
+++
@@ -1,9 +0,0 @@
-package com.capg.fs.dao;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import com.capg.fs.dto.Flight;
-@Repository
-public interface FlightDAO extends JpaRepository<Flight,String>
-{
-
-}
