cba022a73942b430c15dfae3b28a5d4b5a3be751
PallaviReddy27 <62416664+PallaviReddy27@users.noreply.github.com>
2021-01-20 14:48:50 +0530

    Delete FlightScheduleService directory

--- FlightScheduleService/src/main/java/com/capg/fss/dao/FlightScheduleDAO.java
+++
@@ -1,9 +0,0 @@
-package com.capg.fss.dao;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import com.capg.fss.dto.FlightSchedule;
-@Repository
-public interface FlightScheduleDAO  extends JpaRepository<FlightSchedule,String>
-{
-
-}
